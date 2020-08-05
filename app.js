6648ed39d8fd7fdca352c212bec3165705bb62cf
Alekhya <vinnakotaalekhya@gmail.com>
2020-08-05 18:28:06 -0500

    Node Callbacks and Error Handling

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const rect = require('./rectangle');
 
 const doCalculation = (l, w) => {
-    if (l <= 0 || w <= 0) {
-        console.log(`The dimensions of the rectangle are invalid and u passed in ${l} and ${w} as inputs`);
-    }
-    else {
-        console.log(`The Perimater is ${rect.rectangle.perimeter(l, w)} and area is ${rect.rectangle.area(l, w)}`);
-    }
+    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+    rect(l,w,(error,rectangle) => {
+        if(error){
+            console.log('Error:',error.message);
+        }
+        else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    })
+    console.log('This statement is logged after the call to rect()');
 }
 
+
 doCalculation(2,3);
-doCalculation(5,7);
-doCalculation(3,0);
-doCalculation(-2,3);
+
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,16 @@
-exports.rectangle = {
-    perimeter: (x, y) => 2 * (x + y),
-    area: (x, y) => x * y
-} 
+module.exports = (x, y, callback) => {
+    //error handling
+    if (x <= 0 || y <= 0) {
+        //the first argument for the callback fn should always be a error object as per node standards
+        callback(new Error(`The dimensions that you have entered ${x} , ${y} are invalid`))
+    }
+    else {
+        setTimeout(() =>
+            callback(null, {
+                area: () => x * y,
+                perimeter: () => 2 * (x + y)
+            }),
+            2000
+        );
+    }
+}
